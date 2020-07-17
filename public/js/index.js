07176f3740bdc0b5868f6c5ad80be24d90a0a85f
liby5275 <bibinthomas1991@gmail.com>
2020-07-17 15:16:16 +0530

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,41 @@
 const userJoinForm = document.querySelector('#userjoinform')
+const userRegisterForm = document.querySelector('#userRegisterForm')
 
 const userJoinFormButton = userJoinForm.querySelector('button')
 
 userJoinForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    const joinname = userJoinForm.querySelector('#joinname').value
+    const password = userJoinForm.querySelector('#joinpassword').value
 
-    userJoinFormButton.setAttribute('disabled', 'disabled')
+    if(password === "" && joinname.length>0){
+        userJoinForm.querySelector('#joinname').style.display="none"
+        userJoinForm.querySelector('#joinpassword').style.display="block"
+        userJoinForm.querySelector('#nextButton').style.display="none"
+        userJoinForm.querySelector('#joinButton').style.display="block"
 
-    const name = userJoinForm.querySelector('#name').value
-    const genre = userJoinForm.querySelector('#genre').value
-    const author = userJoinForm.querySelector('#author').value
-    const book = userJoinForm.querySelector('#book').value
-    window.location.href="main.html?name=" + name+'?genre='+genre+'?author='+author+'?book='+book;
+    }else {
+        userJoinFormButton.setAttribute('disabled', 'disabled')
+        window.location.href="main.html?name=" + name+'?password='+password;
+    }
+
+    
 })
+
+function register(){
+    userJoinForm.style.display="none"
+    userRegisterForm.style.display="block"
+}
+
+function login() {
+    userJoinForm.style.display="block"
+    userRegisterForm.style.display="none"
+}
+
+function proceedLogin(){
+    userRegisterForm.querySelector('#registername').style.display="none"
+    userRegisterForm.querySelector('#registerpassword').style.display="none"  
+    userRegisterForm.querySelector('#proceedButton').style.display="none"
+    userRegisterForm.querySelector('#confirmpassword').style.display="block"  
+    userRegisterForm.querySelector('#confirmButton').style.display="block"      
+}
